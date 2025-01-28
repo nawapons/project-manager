a3971b22c364c4456bd1762d268a8ad78ae763f1
nawapons <nawapon.s@cvc.ac.th>
2025-01-28 11:08:06 +0700

    adding logo

--- src/app/(standalone)/layout.jsx
+++ src/app/(standalone)/layout.jsx
@@ -1,10 +1,11 @@
 import { UserButton } from "@/features/dashboard/user-button";
+import Image from "next/image";
 const StandloneLayout = ({ children }) => {
     return (
         <main className="bg-neutral-50 min-h-screen">
             <div className="mx-auto max-w-screen-2xl p-4">
                 <nav className="flex justify-between items-center h-[73px]">
-                    Project-Manager
+                    <Image src="/static/images/logo.png" width="170" height="240" />
                     <UserButton />
                 </nav>
                 <div className="flex flex-col items-center justify-center py-4 ">
